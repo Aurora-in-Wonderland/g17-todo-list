30cb88792de385ca6351b12e857e6faff594a779
Aurora-in-Wonderland <eunbipower@naver.com>
2023-06-09 01:01:11 +0900

    통합

--- app.py
+++ app.py
@@ -15,8 +15,12 @@ def home():
 @app.route("/todo", methods=["POST"])
 def todo_post():
     todo_receive = request.form['todo_give']
+    todo_list = list(db.todo.find({}, {'_id': False}))
+    count = len(todo_list) + 1
     doc = {
-        'todo':todo_receive
+        'todo':todo_receive,
+        'num' :count,
+        'done' : 0,
     }
     db.todo.insert_one(doc)
     return jsonify({'msg': '저장완료!'})
@@ -26,6 +30,16 @@ def todo_get():
     all_todos = list(db.todo.find({},{'_id':False}))
 
     return jsonify({'result': all_todos })
+
+# # 진행중인 목록 가져오기-
+def todo_state():
+    todos = list(db.todo.find({},{'state':0}))
+    return jsonify({"actives":todos})
+
+# # 완료된 목록 가져오기
+def todo_state_Done():
+    done = list(db.todo.find({},{'state':1}))
+    return jsonify({"completed":done})
 
 @app.route("/todo", methods=["PUT"])
 def todo_put():
@@ -39,46 +53,19 @@ def todo_put():
     db.users.insert_one(doc)
     db.users.update_one({'todo':todo_receive},{'$set':{'change':change_receive}})
     
-    # 삭제
     return jsonify({'msg': '수정완료!'})
 
-# # 진행중인 목록 가져오기-
-# def todo_state():
-#     todos = list(db.todo.find({},{'state':0}))
-#     return jsonify({"actives":todos})
+@app.route("/todo", methods=["DELETE"])
+def todo_delete():
+    num_receive = request.form["num"]
+    db.todo.delete_one({'num': int(num_receive)})
+    return jsonify({'msg': "삭제 완료!"})
 
-# # 완료된 목록 가져오기
-# def todo_state_Done():
-#     done = list(db.todo.find({},{'state':1}))
-#     return jsonify({"completed":done})
-# if __name__ == "__main__":
-#     app.run("0.0.0.0", port=5001, debug=True)
-
-#[2]
-# @app.route("/todo", methods=["POST"])
-# def todo_post():
-#     todo_receive = request.form['todo_give']
-#     todo_list = list(db.todo.find({}, {'_id': False}))
-#     count = len(todo_list) + 1
-#     doc = {
-#         'todo':todo_receive,
-#         'num' :count,
-#         'done' : 0,
-#     }
-#     db.todo.insert_one(doc)
-#     return jsonify({'msg': '저장완료!'})
-
-# @app.route("/todo", methods=["DELETE"])
-# def todo_delete():
-#     num_receive = request.form["num"]
-#     db.todo.delete_one({'num': int(num_receive)})
-#     return jsonify({'msg': "삭제 완료!"})
-
-# @app.route("/todo/done", methods=["POST"])
-# def todo_done():
-#     num_receive = request.form['done_give']
-#     db.todo.update_one({'num': int(num_receive)}, {'$set': {'done': 1}})
-#     return jsonify({'msg': '계획 완료!'})
+@app.route("/todo/done", methods=["POST"])
+def todo_done():
+    num_receive = request.form['done_give']
+    db.todo.update_one({'num': int(num_receive)}, {'$set': {'done': 1}})
+    return jsonify({'msg': '계획 완료!'})
 
 if __name__ == "__main__":
     app.run("0.0.0.0", port=5001, debug=True)
